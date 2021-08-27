8e2d71ac0f84a7379f1155f9bb5cbf503e4ff961
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-08-27 15:21:17 -0400

    revert changes

--- Develop/pw-gene-setup.js
+++ Develop/pw-gene-setup.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function pwgenerator() {
+
   let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
   let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
   let num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -11,17 +12,15 @@ function pwgenerator() {
 
   let pwlist = [];
 
-  var length = prompt(
-    "How long would you like your password to be? (between 8-128 characters)"
-  );
+
+  var length = prompt("How long would you like your password to be? (between 8-128 characters)");
 
   //length is between 8 and 128 chars
-  if (length == "tofu" || (length >= 8 && length <= 128)) {
+  if (length == "tofu" || length >= 8 && length <= 128) {
+
     //ask the user what characters to include
-    if (length == "tofu") {
-      alert(`${length} is a boomer`);
-      return "";
-    }
+    if (length == "tofu") { alert(`${length} is a boomer`); return "" }
+
 
     var upperCase = confirm("Would you like to use uppercase characters?");
     var numeric = confirm("Would you like to use numeric characters?");
@@ -30,9 +29,8 @@ function pwgenerator() {
 
     //if no characters is selected alert to start over
     if (!upperCase && !numeric && !spCharacter && !lowerCase) {
-      alert("At least one character type must be selected");
-      return "";
-    }
+      alert("At least one character type must be selected"); return ""
+    };
 
     //Makes a pwlist of characters the pw generator will pull from
 
@@ -47,9 +45,10 @@ function pwgenerator() {
 
     //lower if true adds lower into pwlist
     lowerCase == true ? (pwlist = [...lower, ...pwlist]) : "";
+
+
   } else {
-    alert(`${length} is not between 8 and 128`);
-    return "";
+    alert(`${length} is not between 8 and 128`); return ""
   }
 
   //* Generating the Password
@@ -65,25 +64,25 @@ function pwgenerator() {
       //use n as a index to call an array value that is added into generated pw
       generatedpw += pwlist[n];
     }
-  }
+  };
 
   //* Conditions checker
   //I want to check to make sure the generatedpw meets all parameters set by the user
   //if true/true run into pwmaker function again; if true/false check next condition: if false/false check next condition
   //if any characters of .some is included in the generatedpw returns true
 
-  //checks to see if the generatedpw conditions all characters user set
-  while (
-    (a == true && !upper.some((r) => generatedpw.includes(r))) ||
-    (b == true && !num.some((r) => generatedpw.includes(r))) ||
-    (c == true && !spchar.some((r) => generatedpw.includes(r))) ||
-    (d == true && !lower.some((r) => generatedpw.includes(r)))
-  ) {
-    generatedpw = "";
-    pwmaker();
-  }
+  //checks to see if the generated pw contains uppercase character
+  while (upperCase == true && !upper.some((r) => generatedpw.includes(r))) { generatedpw = ""; pwmaker(); };
+  //checks to see if the generated pw contains number
+  while (numeric == true && !num.some((r) => generatedpw.includes(r))) { generatedpw = ""; pwmaker(); };
+  //checks to see if the generated pw contains special character
+  while (spCharacter == true && !spchar.some((r) => generatedpw.includes(r))) { generatedpw = ""; pwmaker(); };
+  //checks to see if generated pw contains lowercase
+  while (lowerCase == true && !lower.some((r) => generatedpw.includes(r))) { generatedpw = ""; pwmaker(); };
+
   //if all conditions are met returns generatedpw
-  return generatedpw;
+  return generatedpw
+
 }
 
 // Write password to the #password input
